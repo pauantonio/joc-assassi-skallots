858b969a5dadd2140127a70a510862705bb47da0
pauantonio <pauantoniosoler@gmail.com>
2025-01-26 16:56:03 +0100

    Add custom PlayerAdminForm to enhance birth_date field input in admin

--- joc/admin.py
+++ joc/admin.py
@@ -8,10 +8,23 @@ from django import forms
 class CsvImportForm(forms.Form):
     csv_file = forms.FileField()
 
+class PlayerAdminForm(forms.ModelForm):
+    class Meta:
+        model = Player
+        fields = '__all__'
+        widgets = {
+            'birth_date': forms.DateInput(format='%d/%m/%Y', attrs={'type': 'date'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['birth_date'].input_formats = ['%d/%m/%Y']
+
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
     change_list_template = "admin/player_changelist.html"
     list_display = ('code', 'first_name', 'last_name', 'birth_date', 'esplai', 'territori_zona')
+    form = PlayerAdminForm
 
     def get_urls(self):
         urls = super().get_urls()
