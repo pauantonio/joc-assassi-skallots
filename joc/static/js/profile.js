c7eb746869d182ea153e0bd30146f6feb4d31294
Pau Antonio <pauantoniosoler@gmail.com>
2025-01-30 23:42:46 +0100

    Enhance profile page code

--- joc/static/js/profile.js
+++ joc/static/js/profile.js
@@ -1,7 +1,8 @@
-const profilePictureInput = document.getElementById('profile-picture-input');
-profilePictureInput.addEventListener('change', function() {
-    if (this.files.length) {
-        const profileForm = document.getElementById('profile-form');
-        profileForm.submit();
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    const profilePictureInput = document.getElementById('profile-picture-input');
+    profilePictureInput.addEventListener('change', () => {
+        if (profilePictureInput.files.length) {
+            document.getElementById('profile-form').submit();
+        }
+    });
 });
