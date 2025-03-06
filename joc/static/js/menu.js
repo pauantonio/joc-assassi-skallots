3c1f054446c6afe6091efa711d6920978abeac0a
pauantonio <pauantoniosoler@gmail.com>
2025-03-06 23:45:12 +0100

    Refactor menu buttons to use anchor tags

--- joc/static/js/menu.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.buttons-list button').forEach(button => {
-        button.addEventListener('click', () => {
-            window.location.href = button.getAttribute('data-url');
-        });
-    });
-});
